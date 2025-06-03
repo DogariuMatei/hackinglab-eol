fea54039f4f245afd4a2a4f4d349f04158850441
George Latsev <g.s.latsev@student.tudelft.nl>
2025-06-03 11:58:40 +0200

    add TLS EOL

--- py_scripts/tls_parser.py
+++ py_scripts/tls_parser.py
@@ -7,10 +7,22 @@ def extract_tls_version(protocol_data):
         handshake_log = tls.get('handshake_log', {})
         server_hello = handshake_log.get('server_hello', {})
         supported_versions = server_hello.get('supported_versions', {})
-        tls_version = supported_versions.get('selected_version', {}).get('name', None) or server_hello.get('version',
-                                                                                                           {}).get(
-            'name', None)
-        return tls_version
+        tls_version = supported_versions.get('selected_version', {}).get('name', None) or \
+                      server_hello.get('version', {}).get('name', None)
+        return tls_version, server_hello.get('server_name', 'unknown')
+    return None, None
+
+def get_clean_tls_version(version):
+    if not version:
+        return version
+    cleaned = version.replace("v", " ").replace("V", " ").strip().split(" ")
+    return cleaned[1]
+
+
+def is_eol_tls(version):
+    if version in ['1.0', '1.1']:
+        return True, '2021-03-01'
+    return False, None
 
 
 def extract_tls_info(json_data):
@@ -22,15 +34,26 @@ def extract_tls_info(json_data):
 
         ip = entry.get('ip')
 
-        for protocol in ['http', 'smtp', 'imap', 'pop3']:
+        for protocol in ['http', 'smtp', 'imap', 'pop3', 'amqp091']:
             protocol_data = entry.get('data', {}).get(protocol, {})
-            tls_version = extract_tls_version(protocol_data)
+            tls_version, server_name = extract_tls_version(protocol_data)
+            clean_tls_version = get_clean_tls_version(tls_version)
 
             if tls_version:
-                results.append({
+                is_eol, eol_from = is_eol_tls(clean_tls_version)
+
+                record = {
                     "ip": ip,
-                    "server": tls_version
-                })
+                    "server": server_name or "unknown",
+                    "version": clean_tls_version,
+                    "api_name": protocol.upper(),
+                    "original_server": tls_version,
+                    "is_eol": is_eol,
+                    "eol_from": eol_from,
+                    "status": f"EOL: {is_eol}, EOL Date: {eol_from or 'N/A'}"
+                }
+
+                results.append(record)
 
     return results
 
